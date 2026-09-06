356af0cac9c100522e69bab3a336bb4dc61add92
Dhairya Baliyan <dhairyabaliyan0812@gmail.com>
2026-09-06 15:30:04 +0530

    feat:set up main routes and base templates and flask login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,32 @@
 from flask import Flask
-from .extensions import db
+from .extensions import db, login_manager
 from .config import Config
+from .models import User 
 
 # Create and configure Flask application
 def create_app():
     app = Flask(__name__)
 
+    # Load configuration from Config class
     app.config.from_object(Config)
-
     db.init_app(app)
 
+    # Flask-Login setup
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"  # your route is auth.login
+    login_manager.login_message = "Please log in."
+    login_manager.login_message_category = "info"
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return db.session.get(User, int(user_id))
+
+    # Register blueprints
+    from .main.routes import main_bp
+    app.register_blueprint(main_bp)
+
     from .auth.routes import auth_bp
-
     app.register_blueprint(auth_bp)
-
-    @app.route('/Home')
-    def home():
-        return "This is our Home page"
 
     #Returning flask application object
     return app
